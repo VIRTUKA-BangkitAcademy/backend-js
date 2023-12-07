4dabaa3e19f9fc76408f6e0d55f3a963a740b9dd
galangw <nganuwijaya05@gmail.com>
2023-12-07 23:03:02 +0700

    random name image

--- src/middleware/multerImage.js
+++ src/middleware/multerImage.js
@@ -1,11 +1,12 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './images/');
+    cb(null, "./images/");
   },
   filename: (req, file, cb) => {
-    cb(null, `${new Date().getTime()}-${file.originalname}`);
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    cb(null, `${uniqueSuffix}.jpg`); // Menggunakan ekstensi file yang diinginkan (misalnya .jpg)
   },
 });
 
