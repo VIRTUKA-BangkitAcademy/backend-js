1f5fa72bef18b021685b00c2b21aa0dbf28751d0
galangw <nganuwijaya05@gmail.com>
2023-12-07 22:38:08 +0700

    fix axios

--- src/controller/analizeFace.controller.js
+++ src/controller/analizeFace.controller.js
@@ -1,25 +1,43 @@
-const axios = require('axios');
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
 
 async function faceAnalize(req, res) {
   const imageBuffer = req.file;
-  try {
-    const images = `http://localhost:8001/images/${imageBuffer.filename}`;
 
-    const respon = await axios.post('http://34.124.232.67:8080/predict', { image: images })
-      .then((response) => {
-        console.log(response);
-      }).catch((error) => {
-        console.log(error);
-      });
+  try {
+    const data = new FormData();
+    data.append("image", fs.createReadStream(imageBuffer.path), {
+      filename: imageBuffer.originalname,
+      contentType: imageBuffer.mimetype,
+    });
+
+    const config = {
+      method: "post",
+      url: "http://34.124.232.67:8080/predict",
+      headers: {
+        ...data.getHeaders(),
+      },
+      data: data,
+    };
+
+    const response = await axios.request(config);
+    console.log(JSON.stringify(response.data));
 
     return res.status(200).json({
-      message: 'success',
-      face: respon,
+      message: "success",
+      face: response.data,
     });
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      // Jika respons dari server ada, tampilkan pesan kesalahan dari server
+      console.error(JSON.stringify(error.response.data));
+    }
+
     return res.status(400).json({
-      message: 'error',
+      message: "error",
     });
   }
 }
